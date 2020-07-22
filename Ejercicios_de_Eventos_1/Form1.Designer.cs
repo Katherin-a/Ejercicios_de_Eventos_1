c7e72d280f5f88ca590bfc4c0ed7be125c34083a
Katherin <Angel@DESKTOP-K23UOE7>
2020-07-22 08:11:21 -0400

    Probando Commit

---
+++ Ejercicios_de_Eventos_1/Form1.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace Ejercicios_de_Eventos_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.TextBox = new System.Windows.Forms.TextBox();
+            this.btn_Agregar = new System.Windows.Forms.Button();
+            this.ListBox = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // TextBox
+            // 
+            this.TextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TextBox.Location = new System.Drawing.Point(21, 28);
+            this.TextBox.Name = "TextBox";
+            this.TextBox.Size = new System.Drawing.Size(224, 21);
+            this.TextBox.TabIndex = 0;
+            // 
+            // btn_Agregar
+            // 
+            this.btn_Agregar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Agregar.Location = new System.Drawing.Point(251, 22);
+            this.btn_Agregar.Name = "btn_Agregar";
+            this.btn_Agregar.Size = new System.Drawing.Size(113, 33);
+            this.btn_Agregar.TabIndex = 1;
+            this.btn_Agregar.Text = "Agregar";
+            this.btn_Agregar.UseVisualStyleBackColor = true;
+            this.btn_Agregar.Click += new System.EventHandler(this.btn_Agregar_Click);
+            // 
+            // ListBox
+            // 
+            this.ListBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ListBox.FormattingEnabled = true;
+            this.ListBox.ItemHeight = 15;
+            this.ListBox.Location = new System.Drawing.Point(21, 73);
+            this.ListBox.Name = "ListBox";
+            this.ListBox.Size = new System.Drawing.Size(343, 139);
+            this.ListBox.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(376, 220);
+            this.Controls.Add(this.ListBox);
+            this.Controls.Add(this.btn_Agregar);
+            this.Controls.Add(this.TextBox);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Ejercicios de Eventos 1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox TextBox;
+        private System.Windows.Forms.Button btn_Agregar;
+        private System.Windows.Forms.ListBox ListBox;
+    }
+}
+
---
+++ Ejercicios_de_Eventos_1/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ejercicios_de_Eventos_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_Agregar_Click(object sender, EventArgs e)
+        {
+            string texto = TextBox.Text;
+
+            ListBox.Items.Add(texto);
+
+            TextBox.Text = "";
+        }
+    }
+}
